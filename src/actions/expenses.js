97b647bb88c996231140ff6113c72d6063d5f61a
Lucas Miranda <lucks-f1sp@hotmail.com>
2019-07-03 19:38:33 -0300

    Setup startRemoveExpense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,42 +1,50 @@
 import uuid from 'uuid';
 import database from '../firebase/firebase';
 
-//ADD EXPENSE
+// ADD_EXPENSE
 export const addExpense = (expense) => ({
-    type: 'ADD_EXPENSE',
-    expense
+  type: 'ADD_EXPENSE',
+  expense
 });
 
 export const startAddExpense = (expenseData = {}) => {
   return (dispatch) => {
     const {
-        description = '',
-        note = '',
-        amount = 0,
-        createdAt = 0
-    } = expenseData;  
+      description = '',
+      note = '',
+      amount = 0,
+      createdAt = 0
+    } = expenseData;
     const expense = { description, note, amount, createdAt };
 
     return database.ref('expenses').push(expense).then((ref) => {
       dispatch(addExpense({
-          id: ref.key,
-          ...expense
+        id: ref.key,
+        ...expense
       }));
     });
   };
 };
 
 // REMOVE_EXPENSE
-export const removeExpense = ({ id} = {}) => ({
+export const removeExpense = ({ id } = {}) => ({
   type: 'REMOVE_EXPENSE',
   id
 });
 
+export const startRemoveExpense = ({ id } = {}) => {
+  return (dispatch) => {
+    return database.ref(`expenses/${id}`).remove().then(() => {
+      dispatch(removeExpense({ id }));
+    });
+  };
+};
+
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
-    type: 'EDIT_EXPENSE',
-    id,
-    updates
+  type: 'EDIT_EXPENSE',
+  id,
+  updates
 });
 
 // SET_EXPENSES
@@ -47,17 +55,17 @@ export const setExpenses = (expenses) => ({
 
 export const startSetExpenses = () => {
   return (dispatch) => {
-   return database.ref('expenses').once('value').then((snapshot) => {
+    return database.ref('expenses').once('value').then((snapshot) => {
       const expenses = [];
 
       snapshot.forEach((childSnapshot) => {
-        expenses.push({ 
+        expenses.push({
           id: childSnapshot.key,
           ...childSnapshot.val()
         });
       });
 
       dispatch(setExpenses(expenses));
-    }); 
+    });
   };
 };
